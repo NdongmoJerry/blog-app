cd14a5983a693e7c666e5fe01eaaa13b6b832163
Collins Tatang <makungongcollins@gmail.com>
2022-07-25 21:44:02 +0100

    new changes

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>My First Blog App</h1>
+    <nav class="nav" id="nav-bar">
+        <ul class nav-list>
+            <li>
+                <a href="#about" id="about">About</a>
+            </li>
+
+    </nav>
+
+</body>
+
+</html>
